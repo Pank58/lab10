32c52614e8b383b7117712608e33e636eb7ad245
PichaevIvan <ivan.pichaev@mail.ru>
2024-12-03 20:08:19 +0300

    add task 2.1

--- lab10/lab10.cpp
+++ lab10/lab10.cpp
@@ -5,35 +5,26 @@
 #include <cstdlib>
 #include <ctime>
 #include <windows.h>
-#include <iomanip>
+#include <climits>
 
 using namespace std;
 
-// Функция для генерации матрицы смежности ориентированного графа
+// Функция для генерации матрицы смежности
 vector<vector<int>> generateAdjMatrix(int n) {
     vector<vector<int>> adjMatrix(n, vector<int>(n, 0));
     srand(time(0));  // Инициализация генератора случайных чисел
 
     // Заполняем матрицу случайными весами рёбер (от 1 до 10)
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i != j && rand() % 2) {  // Ребро существует с вероятностью 50%, не самосоединение
+        for (int j = i + 1; j < n; j++) {
+            if (rand() % 2) {  // Ребро существует с вероятностью 50%
                 int weight = rand() % 10 + 1;  // Вес рёбер от 1 до 10
                 adjMatrix[i][j] = weight;
+                adjMatrix[j][i] = weight;  // Граф неориентированный
             }
         }
     }
     return adjMatrix;
-}
-
-// Функция для вывода матрицы смежности
-void printAdjMatrix(const vector<vector<int>>& adjMatrix) {
-    for (const auto& row : adjMatrix) {
-        for (int val : row) {
-            cout << std::setw(2) << val << " ";
-        }
-        cout << endl;
-    }
 }
 
 // Функция для поиска расстояний от исходной вершины
@@ -62,8 +53,54 @@ vector<int> BFSD(const vector<vector<int>>& adjMatrix, int start) {
     return dist;
 }
 
-int main() {
+// Функция для нахождения радиуса и диаметра
 
+void findRadiusAndDiameter(const vector<vector<int>>& adjMatrix, int& radius, int& diameter, vector<int>& centralVertices, vector<int>& peripheralVertices) {
+    int n = adjMatrix.size();
+    vector<int> allDist(n);
+
+    int maxDist = 0;  // Для вычисления диаметра
+    int minDist = INT_MAX;  // Для вычисления радиуса
+
+    for (int i = 0; i < n; i++) {
+        allDist = BFSD(adjMatrix, i);  // Получаем расстояния от вершины i
+        int maxDistance = 0;
+        for (int d : allDist) {
+            if (d != -1) {
+                maxDistance = max(maxDistance, d);  // Находим максимальное расстояние от вершины i
+            }
+        }
+
+        // Находим диаметр
+        maxDist = max(maxDist, maxDistance);
+
+        // Находим радиус
+        minDist = min(minDist, maxDistance);
+
+        // Добавляем центральные и периферийные вершины
+        if (maxDistance == minDist) {
+            centralVertices.push_back(i);
+        }
+        if (maxDistance == maxDist) {
+            peripheralVertices.push_back(i);
+        }
+    }
+
+    radius = minDist;  // Присваиваем радиус
+    diameter = maxDist;  // Присваиваем диаметр
+}
+
+// Функция для вывода матрицы смежности
+void printAdjMatrix(const vector<vector<int>>& adjMatrix) {
+    for (const auto& row : adjMatrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
@@ -82,7 +119,7 @@ int main() {
         return 1;
     }
 
-    // Генерация случайной матрицы смежности для ориентированного графа
+    // Генерация случайной матрицы смежности
     vector<vector<int>> adjMatrix = generateAdjMatrix(n);
 
     // Вывод сгенерированной матрицы смежности
@@ -102,6 +139,26 @@ int main() {
             cout << "Расстояние до вершины " << i << ": " << dist[i] << endl;
         }
     }
+
+    // Нахождение радиуса, диаметра и центральных/периферийных вершин
+    int radius, diameter;
+    vector<int> centralVertices, peripheralVertices;
+    findRadiusAndDiameter(adjMatrix, radius, diameter, centralVertices, peripheralVertices);
+
+    cout << "\nРадиус графа: " << radius << endl;
+    cout << "Диаметр графа: " << diameter << endl;
+
+    cout << "\nЦентральные вершины: ";
+    for (int v : centralVertices) {
+        cout << v << " ";
+    }
+    cout << endl;
+
+    cout << "\nПериферийные вершины: ";
+    for (int v : peripheralVertices) {
+        cout << v << " ";
+    }
+    cout << endl;
 
     return 0;
 }
